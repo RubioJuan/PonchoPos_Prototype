cd4652b99c79db1aca75c033e631e5ea7cf70e9d
Deivid <deividvelasquez122@gmail.com>
2024-09-25 08:32:35 -0500

    feat: :sparkles: Se Inicio con los servies de cada clase

--- PonchoPOS/src/main/java/com/example/PonchoPOS/service/CategoriaService.java
+++ PonchoPOS/src/main/java/com/example/PonchoPOS/service/CategoriaService.java
@@ -1,4 +1,27 @@
 package com.example.PonchoPOS.service;
 
+import com.example.PonchoPOS.model.Categoria;
+import com.example.PonchoPOS.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    //Obtener todas las categorias
+    public List<Categoria> getAllCategorias(){
+        return categoriaRepository.findAll();
+    }
+
+    //Registras Categorias
+    public Categoria saveCategoria(Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+
+    
 }
