8b992a681361fa3d574b8539500630b0e7ee5bf9
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-09-25 19:32:38 -0500

    refactor: :art: Mejornado repository
    Clientes y facturacion en repository

--- PonchoPOS/src/main/java/com/example/PonchoPOS/repository/ClientesRepository.java
+++ PonchoPOS/src/main/java/com/example/PonchoPOS/repository/ClientesRepository.java
@@ -2,17 +2,6 @@ package com.example.PonchoPOS.repository;
 
 import com.example.PonchoPOS.model.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.List;
 
-public interface ClientesRepository extends JpaRepository<Clientes, Integer> {
-    
-    // Buscar x nombre
-    List<Clientes> findByNombreContainingIgnoreCase(String nombre);
-    
-    // Buscar x ID
-    Optional<Clientes> findById(Integer id);
-
-    // Borrar x id
-    void deleteById(Integer id);
+public interface ClientesRepository extends JpaRepository<Clientes, Integer> { // Usa Integer para que coincida con el tipo de id_cliente
 }
--- PonchoPOS/src/main/java/com/example/PonchoPOS/repository/FacturacionRepository.java
+++ PonchoPOS/src/main/java/com/example/PonchoPOS/repository/FacturacionRepository.java
@@ -3,5 +3,5 @@ package com.example.PonchoPOS.repository;
 import com.example.PonchoPOS.model.Facturacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FacturacionRepository extends JpaRepository<Facturacion, Long> {
-}
+public interface FacturacionRepository extends JpaRepository<Facturacion, Integer> {
+    }
